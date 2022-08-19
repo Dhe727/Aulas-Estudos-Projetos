1572fa9b0403cfb411cbfafda471f02b9ca6dd38
Dhe727 <andrecorrea727@gmail.com>
2022-08-19 19:36:08 -0300

    aula.JS

---
+++ js/JS/scriptAula.js
@@ -0,0 +1,89 @@
+
+function calculaImc (altura, peso){
+    return peso / ( altura * altura);
+}
+
+/*
+var nome = prompt("Qual é Seu Nome?");
+var alturaInformada = prompt(nome + " vamos calcular seu IMC! qual é sua Altura?");
+var pesoInformado = prompt(nome + ",E Qual é seu peso?");
+
+var imc = calculaImc(alturaInformada, pesoInformado);
+
+document.write(nome + ", Seu imc calculado é " + imc);*/
+
+
+
+
+
+
+
+/*
+
+Anotação Java script:
+
+parseInt(); =
+
+é  uma função que está pronta para receber um texto
+e converter para número.....
+
+ex: var vitoria = parseInt(prompt("Quantas Vitória seu time teve?"));
+
+o exemplo acima vai escrever na tela uma pergunta, que recebe como reposta um número,
+porém se não usar a função parseInt() javaScript entende é uma string.
+
+
+
+
+function mostraPontos (){
+    document.write(pontos);
+}
+
+
+var vitoria = parseInt(prompt("Quantas Vitória seu time teve?"));
+var empates = parseInt(prompt("Quantos emapates seu time teve?"));
+
+var pontos = (vitoria * 3) + empates;
+
+if(pontos > 20){
+    document.write(pontos+" pontos, Parabéns!")
+    document.write("seu time está melhor que ano passado! ");
+
+
+}
+if(pontos < 20 ){
+    document.write(pontos +" pontos somente! <br>");
+    document.write("seu time está pior que ano passado!");
+}
+
+
+
+
+
+var numeroPesado = Math.round(Math.random()*5);
+
+
+
+while(numeroPesado != seuChute){
+
+    var seuChute =  parseInt(prompt("Digite o número que Você quer chutar"));
+
+    if (seuChute == numeroPesado){
+        document.write("Parabéns vc Acertou o chute!");
+    }
+    else{
+        alert("não foi dessa vez! tente novamente!");
+    }
+
+}
+*/
+
+
+for(i=0;i<11;i++){
+    document.write("<br>")
+
+    for(b=0;b<10;b++){
+        r = i*b;
+    document.write(i+" x "+b+ " = "+r +"<br>");
+    }
+}
